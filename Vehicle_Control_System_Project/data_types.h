fd3560f8b441db672f673c95294671cc16d04757
Sarah Abuzaid <sarah.muhamedemam@gmail.com>
2023-04-18 02:31:03 +0200

    Init commit

---
+++ Vehicle_Control_System_Project/data_types.h
@@ -0,0 +1,29 @@
+/*
+ * types.h
+ *
+ *  Created on: Mar 7, 2023
+ *      Author: Sarah
+ */
+
+#ifndef DATA_TYPES_H_
+#define DATA_TYPES_H_
+
+
+typedef unsigned char     uint8 ;
+typedef signed char       sint8 ;
+
+typedef unsigned short    uint16;
+typedef signed short      sint16 ;
+
+typedef unsigned long     uint32;
+typedef signed long       sint32;
+
+
+typedef unsigned long long  uint64;
+typedef signed long long    sint64;
+typedef float               float32;
+
+typedef double             float64;
+
+
+#endif /* DATA_TYPES_H_ */
